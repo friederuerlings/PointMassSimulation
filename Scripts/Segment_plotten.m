9952f21ee0afcfb12fbcfd1047ce732047ea086c
uerlingsfrieder <frieder.uerlings@web.de>
2019-11-13 18:18:34 +0100

    13.11.2019 18:18

--- Scripts/Segment_plotten.m
+++ Scripts/Segment_plotten.m
@@ -1,21 +1,29 @@
 pos = sim('segmentCalcPos');
 neg = sim('segmentCalcNeg');
 
-figure(3)
-plot(neg.distance, neg.velocity)
-title('Braking')
-xlabel('distance')
-grid
-hold on
-plot(neg.distance, neg.a_x)
-legend('velocity','acceleration')
-hold off
+% figure(3)
+% plot((length(segments{1,3})-1)-neg.distance, neg.velocity)
+% title('Braking')
+% xlabel('distance')
+% grid
+% hold on
+% plot((length(segments{1,3})-1)-neg.distance, neg.a_x)
+% legend('velocity','acceleration')
+% hold off
 
-figure(4)
-plot(pos.distance, pos.velocity)
-title('Accelerating')
-xlabel('distance')
-grid
+% figure(4)
+% plot(pos.distance, pos.velocity)
+% title('Accelerating')
+% xlabel('distance')
+% grid
+% hold on
+% plot(pos.distance, pos.a_x)
+% legend('velocity','acceleration')
+% hold off
+
+figure(5)
+line((length(segments{1,3})-1)-neg.distance, neg.velocity)
 hold on
-plot(pos.distance, pos.a_x)
-legend('velocity','acceleration')
+line(pos.distance, pos.velocity)
+grid
+

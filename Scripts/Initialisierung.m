05043606a90f944ea37d7ad752f43108a7a60e6b
uerlingsfrieder <frieder.uerlings@web.de>
2019-11-27 19:19:28 +0100

    27.11.2019 19:19

--- Scripts/Initialisierung.m
+++ Scripts/Initialisierung.m
@@ -1,5 +1,7 @@
+clear apexData ggV interpVel resultData segmentData segments vRLookUp
+
 %Gesamtfahrzeug
-init.m            = 270;
+init.m            = 250;
 init.ae_A         = 1.2;
 
 %Aerodynamics
@@ -10,11 +12,13 @@ init.c_D          = 1.3;
 init.my           = 1.3;
 
 %Powertrain
-init.P_engine     = 45; % in kW
+init.P_engine     = 40; % in kW
 
 %Simulation
 init.deltaV       = 5;
 init.ptDistance   = 1; %in Meter
+
+
 
 
 
--- Scripts/filterCourse.m
+++ Scripts/filterCourse.m
@@ -8,5 +8,13 @@ logicMat = horzcat(logicMat,logicMat,logicMat);
 filteredapexData.radius = apexData.radius(logicMat(:,1));
 filteredapexData.velocity = apexData.velocity(logicMat(:,1));
 filteredapexData.locs = apexData.locs(logicMat(:,1));
-filteredapexData.xy(:,1) = apexData.xy(logicMat);
-filteredapexData.xy = horzcat(filteredapexData.xy(1:70,1), filteredapexData.xy(71:140,1), filteredapexData.xy(141:210,1));
+
+x_werte = apexData.xy(:,1);
+y_werte = apexData.xy(:,2);
+r_werte = apexData.xy(:,3);
+
+x_werte = x_werte(logicMat(:,1));
+y_werte = y_werte(logicMat(:,1));
+r_werte = r_werte(logicMat(:,1));
+
+filteredapexData.xy = [x_werte y_werte r_werte];
